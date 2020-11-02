6a692ab31d39d4683d9a2e79a2ec79f7588bc309
guillaume-plantevin <guillaume.plantevin@laplateforme.io>
2020-11-02 15:29:14 +0100

    rajout des job 00 et 01

---
+++ jour01/job00/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="FR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Job 0.9</title>
+</head>
+<body>
+    <?php
+        echo "Hello LaPlateforme!";
+    ?>
+    
+</body>
+</html>
---
+++ jour01/job01/index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Job 1</title>
+</head>
+<body>
+    <?php
+        $str = "LaPlateforme";
+        echo ($str);
+    ?>
+</body>
+</html>
