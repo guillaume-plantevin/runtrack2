f1e94bb78f9b9d57ed8090ec96cce4ac8fef2a3c
guillaume-plantevin <guillaume.plantevin@laplateforme.io>
2020-11-02 17:05:51 +0100

    fin job01 et job02

--- jour01/job01/index.php
+++ jour01/job01/index.php
@@ -1,12 +1,34 @@
 <!DOCTYPE html>
 <html>
-<head>
-    <title>Job 1</title>
-</head>
-<body>
-    <?php
-        $str = "LaPlateforme";
-        echo ($str);
-    ?>
-</body>
+    <head>
+        <title>Job 1</title>
+    </head>
+    <body>
+        <?php
+            $str = "LaPlateforme";
+            echo ($str);
+            echo ("<br />");
+
+            $str2 = "Vive";
+            $str3 = "!";
+
+            echo ($str . $str2 . $str3);
+            echo ("<br />");
+            
+            $val = 6;
+            echo ($val);
+            echo ("<br />");
+
+            $val += 4;
+            echo ($val);
+            echo ("<br />");
+
+            $myBool = TRUE;
+            echo ($myBool);
+            echo("<br />");
+
+            $myBool = false;
+            echo ($myBool);
+        ?>
+    </body>
 </html>
