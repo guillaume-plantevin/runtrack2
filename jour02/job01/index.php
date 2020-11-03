f99328572978998eb6d273a3c793073c57ccc3ac
Guillaume P <guillaume.plantevin@laplateforme.io>
2020-11-03 18:06:24 +0100

    tous les jobs finis

--- jour02/job01/index.php
+++ jour02/job01/index.php
@@ -1,21 +1,21 @@
 <!DOCTYPE html>
 <html lang="fr">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>job 01</title>
-</head>
-<body>
-    <?php
-        $num = 1;
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>job01</title>
+    </head>
+    <body>
+        <?php
+            $num = 0;
 
-        while ($num < 1337) {
-            if ($num == 42)
-                echo "<b><u>" . $num . "</u></b><br />";
-            else
-                echo $num . "<br />";
-            $num+=1;
-        }
-    ?>
-</body>
+            while ($num < 1337) {
+                if ($num == 42)
+                    echo "<b><u>" . $num . "</u></b><br />";
+                else
+                    echo $num . "<br />";
+                $num+=1;
+            }
+        ?>
+    </body>
 </html>
