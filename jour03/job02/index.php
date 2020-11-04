4ed1c1cd4abaca34bae4b7a6dde92f9ed6167656
Guillaume P <guillaume.plantevin@laplateforme.io>
2020-11-04 11:38:59 +0100

    fin jour03: job02

--- jour03/job02/index.php
+++ jour03/job02/index.php
@@ -6,6 +6,15 @@
     <title>Jour03: job 02</title>
 </head>
 <body>
+    <?php
+       $batty = "Tous ces instants seront perdus dans le temps comme les larmes sous la pluie.";
+        //    recuparaiton de la longueur de la chaine
+       $i = strlen($batty);
+       
+        //    utilisation pour fixer la limite de la for-loop
+        for ($j = 0; $j < $i; $j += 2) 
+            echo $batty[$j];
+    ?>
     
 </body>
 </html>
