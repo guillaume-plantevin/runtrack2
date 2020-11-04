627cf7de078e5e40f4fc2ed92887ef5c23803639
Guillaume P <guillaume.plantevin@laplateforme.io>
2020-11-04 12:24:27 +0100

    correction mineure de jour03: job02

--- jour03/job02/index.php
+++ jour03/job02/index.php
@@ -8,7 +8,7 @@
 <body>
     <?php
        $batty = "Tous ces instants seront perdus dans le temps comme les larmes sous la pluie.";
-        //    recuparaiton de la longueur de la chaine
+        //    recuparation de la longueur de la chaine
        $i = strlen($batty);
        
         //    utilisation pour fixer la limite de la for-loop
--- jour03/job03/index.php
+++ jour03/job03/index.php
@@ -6,6 +6,16 @@
     <title>Jour03: job 03</title>
 </head>
 <body>
+    <?php
+        $hal = "I'm sorry Dave I'm afraid I can't do that";
+        $str_len = strlen($hal);
+        echo $str_len . "<br />";
+
+        for ($i = 0; $i < $str_len; ++$i) {
+            if (($hal[$i] >= "a" && $hal[$i] <= "z") && ($hal[$i] >= "A" && $hal[$i] <= "Z"))
+                echo $hal[$i];
+        }
+    ?>
     
 </body>
 </html>
