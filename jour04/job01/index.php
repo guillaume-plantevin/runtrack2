6dc81cd029e30c8f1fa50b7b1d15a4844f5c27e9
guillaume-plantevin <guillaume.plantevin@laplateforme.io>
2020-11-06 08:06:48 +0100

    jour04: job01 02 03 finis

--- jour04/job01/index.php
+++ jour04/job01/index.php
@@ -4,54 +4,32 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>jour04: job01</title>
-        <style>
-            fieldset {
-                font-size: 1.3em;
-                border: 4px solid black;
-            }
-            p {
-                text-align: center;
-            }
-            fieldset input {
-                height: 2em;
-                margin-top: 2em;
-            }
-            label {
-                display: inline-block;
-                width: 200px;
-            }
-            fieldset {
-                margin: 5% auto;
-                width: 50%;
-            }
-            #sub{
-                border-radius: 15px;
-                margin-top: 1em;
-                font-size: .7em;
-                height: 2.5em;
-                background: pink;
-            }
-        </style>
     </head>
     <body>
-        <form action="index.php" method="get">
-            <fieldset>
-            <legend>Formulaire GET</legend>
-            <label for="name">Nom:</label>
-            <input type="text" name="name"><br>
-            <label for="email">Email:</label>
-            <input type="text" name="email"><br>
-            <input id= "sub" type="submit">
-            </fieldset>
+        <form action = "" method = "get">
+        <!-- <form action = "" method = "post"> -->
+            prenom: <br />
+            <input type = "text" name = "firstname" value = "Jean">
+            <br /><br />
+            Nom: <br />
+            <input type = "text" name = "lastname" value = "Dupont">
+            <br /><br />
+            Sujet favori<br />          
+            <input type = "checkbox" name = "suj" value = "ALGO">Algorithme
+            <input type = "checkbox" name = "suj" value = "HTML">HTML
+            <input type = "checkbox" name = "suj" value = "CSS">CSS
+            <input type = "checkbox" name = "suj" value = "PHP">PHP
+            <br /><br />
+            Sexe: <br />
+            <input type = "radio" name = "gender" value = "M">Homme
+            <input type = "radio" name = "gender" value = "F">Femme
+            <br /><br />
+            <input type = "submit" name="sm" value = "Envoyer">     
         </form>
+
         <?php
-        $arr = ["bonjour", "au revoir", "salut"];
-        $int = sizeof($arr);
-            var_dump($arr);
-            echo "<br> nombre d'entrée de \$int: " . $int . "<br />";
-            // foreach ()
-            // echo "<p> your name is " . 
-            // echo "<p> your email is " . $_GET["email"] . "</p>";
+           if (isset($_GET['sm']))
+                echo "Il y a " . count($_GET) . " arguments.<br />";
         ?>
         
     </body>
