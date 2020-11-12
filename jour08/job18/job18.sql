9f381558dabb3614579164af77f38a047ff5dcb6
guillaume-plantevin <71876345+guillaume-plantevin@users.noreply.github.com>
2020-11-12 14:47:38 +0100

    Update job18.sql

--- jour08/job18/job18.sql
+++ jour08/job18/job18.sql
@@ -1,2 +1,6 @@
-# pas encore fini
-SELECT MAX(capacite) AS 'Biggest Room', nom FROM salles 
+SELECT etage.id, salles.nom AS 'Biggest Room' , MAX(salles.capacite) AS 'capacite'
+FROM `salles`
+INNER JOIN
+etage
+ON 
+salles.id_etage = etage.id;
