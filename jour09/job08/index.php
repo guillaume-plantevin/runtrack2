05f36f4e87a50bdd18e1de283c0cbac6e73564e8
Guillaume P <guillaume.plantevin@laplateforme.io>
2020-11-16 13:46:44 +0100

    jour09: job11 fini

--- jour09/job08/index.php
+++ jour09/job08/index.php
@@ -58,11 +58,3 @@
         </table>
     </body>
 </html>
-<!-- 
-    En utilisant php, connectez-vous à la base de données “jour08”. A l’aide
-    d’une requête SQL, sélectionnez dans une colonne nommée
-    “capacite_totale” la somme des capacités des salles. Affichez le résultat de
-    cette requête dans un tableau html. La première ligne de votre tableau html
-    doit contenir le nom des champs. Les suivantes doivent contenir les
-    données présentes dans votre base de données.
- -->
