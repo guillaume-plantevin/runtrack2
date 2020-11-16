1b4773a38aaa9e260b9a858cd05680ab1f8f32d6
Guillaume P <guillaume.plantevin@laplateforme.io>
2020-11-16 13:17:06 +0100

    suppression des consignes dans job03

--- jour09/job03/index.php
+++ jour09/job03/index.php
@@ -60,11 +60,3 @@
         </table>
     </body>
 </html>
-<!-- 
-    En utilisant php et mysqli, connectez-vous à la base de données “jour08”. A
-    l’aide d’une requête SQL, récupérez le prenom, le nom et la date de
-    naissance des étudiants de sexe féminin. Affichez le résultat de cette
-    requête dans un tableau html. La première ligne de votre tableau html doit
-    contenir le nom des champs. Les suivantes doivent contenir les données
-    présentes dans votre base de données.
- -->
