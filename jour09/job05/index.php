368656b1d284280f406e2c3f6b067938aeab02e2
Guillaume P <guillaume.plantevin@laplateforme.io>
2020-11-16 13:22:28 +0100

    jour09: job05 fini

--- jour09/job05/index.php
+++ jour09/job05/index.php
@@ -1,8 +1,65 @@
-<!-- 
-    En utilisant php, connectez-vous à la base de données “jour08”. A l’aide
-    d’une requête SQL, récupérez l’ensemble des informations des étudiants
-    qui ont moins de 18 ans. Affichez le résultat de cette requête dans un
-    tableau html. La première ligne de votre tableau html doit contenir le nom
-    des champs. Les suivantes doivent contenir les données présentes dans
-    votre base de données.
- -->
+<?php
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+    $database = 'jour08';
+
+    // creer la connexion, retour negatif = false
+    $connexion = mysqli_connect($servername, $username, $password, $database);
+
+    // verifier la connexion
+    if (!$connexion)
+        die("probleme de connexion: " . mysqli_connect_error());
+        
+    $requete = "SELECT * FROM `etudiants` WHERE YEAR(NOW()) - YEAR(naissance) < 18";
+    $premconn = mysqli_query($connexion, $requete);
+?>
+<!DOCTYPE html>
+<html lang="fr">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>jour09: job01</title>
+        <style>
+            table, th, td {
+                border: 2px solid black;
+            }
+            table {
+                border-collapse: collapse;
+            }
+            th {
+                text-transform: uppercase;
+            }
+            th, td {
+                padding: 1em;
+            }
+        </style>
+    </head>
+    <body>
+        <table>
+            <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>Prenom</th>
+                    <th>Nom</th>
+                    <th>Naissance</th>
+                    <th>Sexe</th>
+                    <th>Email</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php
+                    while ($row = mysqli_fetch_array($premconn)) {
+                        echo '<tr>';
+                        for ($i = 0; isset($row[$i]); ++$i) {
+                            echo '<td>' . $row[$i] . '</td>';
+                        }
+                        echo '</tr>';
+                    }
+
+                    mysqli_close($connexion);
+                ?>
+            </tbody>
+        </table>
+    </body>
+</html> 
