4ee19b1761f0da0026354db384b9376464517a4c
Guillaume P <guillaume.plantevin@laplateforme.io>
2020-11-16 13:48:54 +0100

    jour09: job12 fini

--- jour09/job12/index.php
+++ jour09/job12/index.php
@@ -1,8 +1,62 @@
-<!-- 
-    En utilisant php, connectez-vous à la base de données “jour08”. A l’aide
-    d’une requête SQL, sélectionnez le prenom, le nom et la date de naissance
-    des étudiants qui sont nés entre 1998 et 2018. Affichez le résultat de cette
-    requête dans un tableau html. La première ligne de votre tableau html doit
-    contenir le nom des champs. Les suivantes doivent contenir les données
-    présentes dans votre base de données.
- -->
+<?php
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+    $database = 'jour08';
+
+    // creer la connexion, retour negatif = false
+    $connexion = mysqli_connect($servername, $username, $password, $database);
+
+    // verifier la connexion
+    if (!$connexion)
+        die("probleme de connexion: " . mysqli_connect_error());
+        
+    $requete = "SELECT prenom, nom, naissance FROM etudiants WHERE YEAR(naissance) BETWEEN 1998 AND 2018";
+    $premconn = mysqli_query($connexion, $requete);
+?>
+<!DOCTYPE html>
+<html lang="fr">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>jour09: job12</title>
+        <style>
+            table, th, td {
+                border: 2px solid black;
+            }
+            table {
+                border-collapse: collapse;
+            }
+            th {
+                text-transform: uppercase;
+            }
+            th, td {
+                padding: 1em;
+            }
+        </style>
+    </head>
+    <body>
+        <table>
+            <thead>
+                <tr>
+                    <th>Prenom</th>
+                    <th>Nom</th>
+                    <th>Naissance</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php
+                    while ($row = mysqli_fetch_array($premconn)) {
+                        echo '<tr>';
+                        for ($i = 0; isset($row[$i]); ++$i) {
+                            echo '<td>' . $row[$i] . '</td>';
+                        }
+                        echo '</tr>';
+                    }
+
+                    mysqli_close($connexion);
+                ?>
+            </tbody>
+        </table>
+    </body>
+</html>
